800261595c923e6f5d3d553bf8d3e87f1b6db00b
Leiner117 <lealvarado@estudiantec.cr>
2022-09-16 20:12:30 -0600

    update 3

--- proyecto/include/People.h
+++ proyecto/include/People.h
@@ -5,15 +5,36 @@ using namespace std;
 class People
 {
     public:
-        string name;// Nombre
-        string id;// Cedula
-        short age;// edad
-        string placeResidence;// Lugar de residencia
-        string yearIncome;// Año ingreso
-        void setName(string);
+
+
+        //Contructor
         People(string,string,short,string,string);
+
+        //Funciones
         void print();
-        virtual ~People();
+         //Setters
+        void setName(string);
+        void setId(string);
+        void setAge(short);
+        void setPlaceResidence(string);
+        void setYearIncome(string);
+         //Getters
+        string getName();
+        string getId();
+        short getAge();
+        string getPlaceResidence();
+        string getYearIncome();
+
+
+    private:
+            //Atributos
+            string name;// Nombre
+            string id;// Cedula
+            short age;// edad
+            string placeResidence;// Lugar de residencia
+            string yearIncome;// Año ingreso
+
+
 
 
 };
--- proyecto/src/People.cpp
+++ proyecto/src/People.cpp
@@ -5,15 +5,52 @@ using namespace std;
 People::People(string n,string id,short age,string place,string year)
 {
     setName(n);
+    setId(id);
+    setAge(age);
+    setPlaceResidence(place);
+    setYearIncome(year);
+
 }
 
 void People::setName(string n){
     name = n;
 }
+
+
+void People::setId(string i){
+    id = i;
+}
+void People::setAge(short ag){
+    age = ag;
+}
+void People::setPlaceResidence(string place){
+    placeResidence = place;
+}
+void People::setYearIncome(string year){
+
+    yearIncome = year;
+}
+
+
+string People::getId(){
+    return id;
+}
+string People::getName(){
+    return name;
+}
+
+short People::getAge(){
+    return age;
+}
+string People::getPlaceResidence(){
+    return placeResidence;
+}
+
+string People::getYearIncome(){
+    return yearIncome;
+}
+
 void People::print(){
-    cout<<"Prueba";
+    cout<<getName();
 }
-People::~People()
-{
-    //dtor
-}
+
